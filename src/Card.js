34b4f328cb1479b249aba264bd41ed2e9dfe7bca
Fernando Comet <fernandocomet@gmail.com>
2021-04-02 19:24:41 +0200

    changing imgs path

--- src/Card.js
+++ src/Card.js
@@ -6,16 +6,16 @@ import { ExternalLink } from 'react-external-link';
 */
 
 class Card extends Component{
+   
 
     render(){
         return(
-            <div className="card1">
-            Hola esto es  Card 
-            <header className="card-header1">
-                <img src={this.props.image} alt={this.props.resourceName} className="card-header" width='720px'/>
-            </header>
+            <div className="card">
+                <div className="card-header">
+                    <img src={this.props.image} alt={this.props.resourceName} width='720px'/>
+                </div>
                 <div>
-                    <h2>{this.props.idx}</h2>
+                    <h2>Card</h2>
                     <ExternalLink href={this.props.resourceLink}>
                         <h1>{this.props.resourceName}</h1>
                     </ExternalLink>
